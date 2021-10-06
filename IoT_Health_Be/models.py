6e7ae4cb65a3a35734e26cce24e4de0812a3b5ae
pgoseco <patrickgoseco@gmail.com>
2021-10-06 14:54:34 +1100

    Made initial models for covid patient

--- IoT_Health_Be/models.py
+++ IoT_Health_Be/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    covid_positive = models.BooleanField(default=True)
+    height = models.FloatField()
+    weight = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Device(models.Model):
+    person = models.ForeignKey(Person, related_name='devices',
+                             null=True, on_delete=models.SET_NULL)
+    deviceId = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.deviceId
+
+
+class VitalSigns(models.Model):
+    device = models.ForeignKey(Device, related_name='vitalsigns',
+                             on_delete=models.CASCADE)
+    covid_symptoms = models.BooleanField(default=True)
+    body_temperature = models.FloatField()
+    pulse_rate = models.FloatField()
+    respiration_rate = models.FloatField()
+    blood_pressure = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.covid_symptoms
